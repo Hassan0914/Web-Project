61d391b7d9ba3b79ed840c9d65fc736880bfc639
Hassan0914 <educationtech1211@gmail.com>
2024-10-08 09:10:23 -0700

    Created a new folder for game assets

--- jhhj.js
+++ jhhj.js
@@ -1,7 +1,7 @@
 console.log("Welcome to TIC TIC TOE")
-let music = new Audio("ting.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("ting.mp3")
+let music = new Audio("assets/ting.mp3")
+let audioTurn = new Audio("assets/ting.mp3")
+let gameover = new Audio("assets/ting.mp3")
 let turn = "X"
 let isgameover = false;
 //Function to change the turn
